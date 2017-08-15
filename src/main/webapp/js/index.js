45bd0476eda614b6c08e495779a24eb54f20a3d1
kris <qqkris@163.com>
2017-08-15 15:31:28 +0800

    修正文件下载接口

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -387,7 +387,7 @@
                                     downBtn.hide();
                                 } else if (file.status === 1)
                                 {
-                                    downBtn.attr("href", syfm.apiUriRoot + "fileDown?fileid=" + file.hash);
+                                    downBtn.attr("href", syfm.apiUriRoot + "fileDown?fileid=" + file.uuid+"&userid="+file.userid);
                                     downBtn.show();
                                     goonInputBox.hide();
                                 }
@@ -420,7 +420,7 @@
                                         downBtn.hide();
                                     } else if (file.status === 1)
                                     {
-                                        downBtn.attr("href", syfm.apiUriRoot + "fileDown?fileid=" + file.hash);
+                                       downBtn.attr("href", syfm.apiUriRoot + "fileDown?fileid=" + file.uuid+"&userid="+file.userid);
                                         downBtn.show();
                                         goonInputBox.hide();
                                     }
